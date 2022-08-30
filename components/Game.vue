46feda96f22f53d02ce794b252cf06ffa8e46ce1
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-08-30 14:39:57 +0200

    init projet

---
+++ components/Game.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+    <h1>Bienvenue sur le site </h1>
+    
+    <h2>Liste des jeux</h2>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Game'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Game/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
