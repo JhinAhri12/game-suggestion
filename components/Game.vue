c473264232ca1f013b5827bdfd80fe568bd7a411
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-08-30 14:52:50 +0200

    add squelette index

--- components/Game.vue
+++ components/Game.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="container">
-    <h1>Bienvenue sur le site </h1>
-    
-    <h2>Liste des jeux</h2>
+        <h1>Bienvenue sur le site </h1>
+        
+        <button class="btn btn-primary">Créer un sondage</button>
+
+        <form action="">
+            <input type="text" placeholder="Question de votre sondage">
+            <button class="btn btn-primary">Ajouter un choix</button>
+        </form>
+
+        <h2>Liste des jeux</h2>
     </div>
 </template>
 <script>
